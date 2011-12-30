440ec44c652954c1c639c5d04b4ed44dee3a7f67
Nomad <gd.nightmare@gmail.com>
2011-12-30 20:30:38 +0200

    Added more comments, corrected dealocation, minor other changes. Should check if it works correctly...

--- BucketSort/main.cpp
+++ BucketSort/main.cpp
@@ -36,12 +36,12 @@ int main()
 	    cout << setw(15) << array[c] << ((c+1)%5 ? "" : "\n");
 	cout << endl;
 	
-	//bucketsort(array, SIZE);	// sort array
+	bucketsort(array, SIZE);	// sort array
 	
 	cout << "The same array sorted:\n\n";
 	//for( c = 0 ; c < 5 ; c++ )	// print a header?
 	//    cout << "     9876543210";
-	cout << '|' << endl;
+	//cout << '|' << endl;
 	for(c = 0 ; c < SIZE ; c++)	// display array (5 numbers per row)
 	    cout << setw(15) << array[c] << ((c+1)%5 ? "" : "\n");
 	cout << endl;
@@ -51,7 +51,7 @@ int main()
 } // end function main
 
 
-void bucketsort(unsigned int *current, unsigned int size)
+void bucketsort(unsigned int *current, unsigned int size)	// for little-endian architectures
 {
 	unsigned int *end;
 	unsigned char *byte;
@@ -61,13 +61,15 @@ void bucketsort(unsigned int *current, unsigned int size)
 	queue<unsigned int> *lowerEnd;
 	size_t digit;
 	unsigned int number;
-	void *temp;
+	queue<unsigned int> *temp;
 	
+	// initialize arrays of queues
 	upper = new queue<unsigned int> [256];
 	lower = new queue<unsigned int> [256];
 	upperEnd = upper + 256;
 	lowerEnd = lower + 256;
 	
+	// make first pass dropping array elements into buckets.
 	end = current + size;
 	byte = (unsigned char*)current;
 	while(current < end)
@@ -90,39 +92,47 @@ void bucketsort(unsigned int *current, unsigned int size)
 //	}
 //	upper = upperEnd - 256;
 //	//
+
 	digit = 0;
-	while( ++digit < sizeof(unsigned int) )
+	while( ++digit < sizeof(unsigned int) )	// at the end of each iteration lower has the data.
 	{
 		byte = (unsigned char*)&number + digit;
 		
-		while( upper < upperEnd )
+		while( upper < upperEnd )	// for each bucket in upper array of buckets
 		{
-			while( !upper->empty() )
+			while( !upper->empty() )	// while bucket is not empty
 			{
-				number = upper->front();
-				upper->pop();
-				lower[*byte].push(number);
-			}
+				number = upper->front();	// remove number from...
+				upper->pop();				// ...upper bucket
+				lower[*byte].push(number);	// drop it in lower bucket
+			} // end while
 			upper++;
-		}
+		} // end while
+
+		// restore upper. swap upper/lower & upperEnd/lowerEnd.
 		upper = lower;
-		lower = upperEnd - 256;
+		lower = upperEnd - 256;	// restore lower
 		temp = upperEnd;
 		upperEnd = lowerEnd;
-		lowerEnd = (queue<unsigned int> *)temp;
+		lowerEnd = temp;
 	} // end while
 	
-	free(lower);
-	current = end - size;
-	while( upper < upperEnd )
+	delete[] lower;
+
+	// copy elements to initial array
+	current = end - size;	// restore current
+	while( upper < upperEnd )	// for each bucket in upper array of buckets
 	{
-		while( !upper->empty() )
+		while( !upper->empty() )	// while bucket is not empty
 		{
 			*current++ = upper->front();
 			upper->pop();
-		}
+		} // end while
 		upper++;
 	} // end while
+
+	upper = upperEnd - 256;	// restore upper
+	delete[] upper;
 
 	
 //	c = 0;
@@ -134,5 +144,5 @@ void bucketsort(unsigned int *current, unsigned int size)
 //		buckets[0].pop();
 //	}
 //	cout << endl;
-	
+
 } // end function bucketsort
